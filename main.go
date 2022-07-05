4d872e907d71b29762a6c222d3877da1b3e4d556
NRO1 <nrdevac1@gmail.com>
2022-07-05 14:34:24 +0300

    05072022_1

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
 func main() {
+	startGame()
+
+	winner := "" //"Player" || "Monster" || ""
+
+	//this loop will run while there is no winner, and end when we have a winner
+	for winner == "" {
+		winner = executeRound()
+	}
+
+	endGame()
 
 }
+
+func startGame() {}
+
+func executeRound() string {}
+
+func endGame() {}
